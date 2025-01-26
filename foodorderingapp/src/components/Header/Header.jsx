d59e1300dc8fd0bafe7361aac3882af5bf07e0b0
Oliver Bielik <oliverbielik@gmail.com>
2025-01-26 19:02:48 +0100

    added x to close search and overlay when search opened

--- foodorderingapp/src/components/Header/Header.jsx
+++ foodorderingapp/src/components/Header/Header.jsx
@@ -36,7 +36,11 @@ function Header({ onSearch }) {
   };
 
   const handleSearchClick = () => {
-    setIsSearchExpanded((prev) => !prev);
+    setIsSearchExpanded(true);
+  };
+
+  const closeSearch = () => {
+    setIsSearchExpanded(false);
   };
 
   function getLowestPrice(restaurant) {
@@ -45,71 +49,84 @@ function Header({ onSearch }) {
   }
 
   return (
-    <div
-      className={`${styles.headerRow} ${
-        isSearchExpanded ? styles.expanded : ""
-      }`}
-    >
-      {!isSearchExpanded && (
-        <div className={styles.titleLocation}>
-          <p
-            className={`${styles.titleText} ${styles.unselectable}`}
-            onClick={redirectToHomepage}
-          >
-            Foodie
-          </p>
-          <Location />
-        </div>
+    <div className={styles.headerContainer}>
+      {isSearchExpanded && (
+        <div className={styles.shadowOverlay} onClick={closeSearch}></div>
       )}
-      <div className={styles.searchColumn}>
-        <SearchBar
-          onSearch={updateFilteredRestaurants}
-          onSearchClick={handleSearchClick}
-        />
-        {isSearchExpanded && filteredRestaurants.length > 0 && (
-          <div className={styles.restaurantListWrapper}>
-            <div className={styles.restaurantList}>
-              {filteredRestaurants.map((restaurant, index) => (
-                <div key={index} className={styles.restaurantItem}>
-                  <div
-                    className={styles.concreteRestaurantTile}
-                    onClick={() => goToRestaurant(restaurant.id)}
-                  >
-                    <div className={styles.row}>
-                      <div>{restaurant.name}</div>|
+
+      <div
+        className={`${styles.headerRow} ${
+          isSearchExpanded ? styles.expanded : ""
+        }`}
+      >
+        {!isSearchExpanded && (
+          <div className={styles.titleLocation}>
+            <p
+              className={`${styles.titleText} ${styles.unselectable}`}
+              onClick={redirectToHomepage}
+            >
+              Foodie
+            </p>
+            <Location />
+          </div>
+        )}
+        <div className={styles.searchColumn}>
+          <div className={styles.searchBarWrapper}>
+            <SearchBar
+              onSearch={updateFilteredRestaurants}
+              onSearchClick={handleSearchClick}
+            />
+            {isSearchExpanded && (
+              <button className={styles.closeButton} onClick={closeSearch}>
+                ✖
+              </button>
+            )}
+          </div>
+          {isSearchExpanded && filteredRestaurants.length > 0 && (
+            <div className={styles.restaurantListWrapper}>
+              <div className={styles.restaurantList}>
+                {filteredRestaurants.map((restaurant, index) => (
+                  <div key={index} className={styles.restaurantItem}>
+                    <div
+                      className={styles.concreteRestaurantTile}
+                      onClick={() => goToRestaurant(restaurant.id)}
+                    >
                       <div className={styles.row}>
-                        <div>{restaurant.rating} </div>
-                        <div className={styles.star}>&#9733;</div>
+                        <div>{restaurant.name}</div>|
+                        <div className={styles.row}>
+                          <div>{restaurant.rating} </div>
+                          <div className={styles.star}>&#9733;</div>
+                        </div>
                       </div>
+                      <div>Food from: {getLowestPrice(restaurant)} kč</div>
                     </div>
-                    <div>Food from: {getLowestPrice(restaurant)} kč</div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
+        {!isSearchExpanded && (
+          <div className={styles.menuItems}>
+            <div>
+              <ul>
+                <li>
+                  <div
+                    className={styles.menuItemsStyle}
+                    onClick={redirectToPaymant}
+                  >
+                    {getTotalOrders()} Cart
+                  </div>
+                </li>
+                <li></li>
+                <li>
+                  <div className={styles.menuItemsStyle}>Menu</div>
+                </li>
+              </ul>
             </div>
           </div>
         )}
       </div>
-      {!isSearchExpanded && (
-        <div className={styles.menuItems}>
-          <div>
-            <ul>
-              <li>
-                <div
-                  className={styles.menuItemsStyle}
-                  onClick={redirectToPaymant}
-                >
-                  {getTotalOrders()} Cart
-                </div>
-              </li>
-              <li></li>
-              <li>
-                <div className={styles.menuItemsStyle}>Menu</div>
-              </li>
-            </ul>
-          </div>
-        </div>
-      )}
     </div>
   );
 }
